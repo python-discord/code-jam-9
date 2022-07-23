cb3559e78b8ce249934faaf73ec55de234f9ddb5
ilcheese2 <cheddarcheese1238@gmail.com>
2022-07-23 12:02:04 -0700

    Fixed server.py. Created buggy client.py. Client needs fixing

--- backend/server.py
+++ backend/server.py
@@ -47,31 +47,39 @@ class Server:
     async def handler(self, websocket):
         """Handle websocket connections."""
         if len(self.active_clients) >= MAX_PLAYERS:
-            await websocket.close(reason='Only four players at once!')
+            await websocket.close(reason="Only four players at once!")
 
         player = Player(websocket.id, len(self.active_clients), websocket)
         self.active_clients[player.client_id] = player
+        await websocket.send(json.dumps({
+            "type": "info",
+            "data": {
+                "screen_width": SCREEN_WIDTH,
+                "screen_height": SCREEN_HEIGHT,
+                "paddle_length": PADDLE_LENGTH,
+                "paddle_width": PADDLE_WIDTH,
+                "paddle_num": len(self.active_clients) - 1
+            }
+        }))
         async for message in websocket:
             event = json.loads(message)
 
             # print(websocket.id, event)  # DEBUG
 
-            if event['type'] == 'init':
-                await websocket.send(json.dumps({'paddle_num': player.player_number}))
-            elif event['type'] == 'paddle':
+            if event["type"] == "paddle":
                 # Paddle position update
-                self.paddle_update_handler(player, event['data'])
+                self.paddle_update_handler(player, event["data"])
                 # await player.connection.send(json.dumps(player.paddle_position))
         # Clean up the connection
         del self.active_clients[websocket.id] # Clear client num
 
     async def main(self):
         """Process a game loop tick."""
-        async with websockets.serve(self.handler, 'localhost', 8765):
+        async with websockets.serve(self.handler, "localhost", 8765):
             while True:
                 await asyncio.sleep(1 / 60)
-                self.game_update()
-                self.broadcast_updates()
+                await self.game_update()
+                await self.broadcast_updates()
 
     def reset_ball(self):
         """Reset the ball position."""
@@ -86,26 +94,26 @@ class Server:
 
         # Check if ball collides with a wall
         if ball_x <= 0:
-            if self.client_nums[0] is not None:
-                self.client_scores[self.last_paddle_bounced] += 1
+            if len(self.active_clients) > 0:
+                self.client_from_side(self.last_paddle_bounced).score += 1
                 self.reset_ball()
             else:
                 collided_side = 0
         if ball_x >= SCREEN_WIDTH:
-            if self.client_nums[1] is not None:
-                self.client_scores[self.last_paddle_bounced] += 1
+            if len(self.active_clients) > 1:
+                self.client_from_side(self.last_paddle_bounced).score += 1
                 self.reset_ball()
             else:
                 collided_side = 1
         if ball_y <= 0:
-            if self.client_nums[2] is not None:
-                self.client_scores[self.last_paddle_bounced] += 1
+            if len(self.active_clients) > 2:
+                self.client_from_side(self.last_paddle_bounced).score += 1
                 self.reset_ball()
             else:
                 collided_side = 2
         if ball_y >= SCREEN_HEIGHT:
-            if self.client_nums[3] is not None:
-                self.client_scores[self.last_paddle_bounced] += 1
+            if len(self.active_clients) > 3:
+                self.client_from_side(self.last_paddle_bounced).score += 1
                 self.reset_ball()
             else:
                 collided_side = 3
@@ -125,13 +133,13 @@ class Server:
             self.ball_bounced = True
             # Calculate new ball speed
             if collided_side == 0:
-                self.ball_speed(-self.ball_speed[0], self.ball_speed[1])
+                self.ball_speed = (-self.ball_speed[0], self.ball_speed[1])
             if collided_side == 1:
-                self.ball_speed(-self.ball_speed[0], self.ball_speed[1])
+                self.ball_speed = (-self.ball_speed[0], self.ball_speed[1])
             if collided_side == 2:
-                self.ball_speed(self.ball_speed[0], -self.ball_speed[1])
+                self.ball_speed = (self.ball_speed[0], -self.ball_speed[1])
             if collided_side == 3:
-                self.ball_speed(self.ball_speed[0], -self.ball_speed[1])
+                self.ball_speed = (self.ball_speed[0], -self.ball_speed[1])
         else:
             self.ball_bounced = False
 
@@ -151,42 +159,48 @@ class Server:
         """
         ball_x = self.ball_position[0]
         ball_y = self.ball_position[1]
-        if (ball_y >= self.client_positions[0][1]
-                and ball_y <= self.client_positions[0][1] + PADDLE_LENGTH
-                and ball_x <= self.client_positions[0][0]
-                and ball_x >= self.client_positions[0][0] - PADDLE_WIDTH):
+        clients = [self.client_from_side(i) for i in range(4)]
+        if (ball_y >= clients[0].paddle_position[1]
+                and ball_y <= clients[0].paddle_position[1] + PADDLE_LENGTH
+                and ball_x <= clients[0].paddle_position[0]
+                and ball_x >= clients[0].paddle_position[0] - PADDLE_WIDTH):
             return 0
-        elif (ball_y <= self.client_positions[1][1]
-                and ball_y >= self.client_positions[1][1] - PADDLE_LENGTH
-                and ball_x >= self.client_positions[1][0]
-                and ball_x <= self.client_positions[1][0] + PADDLE_WIDTH):
+        elif (ball_y <= clients[1].paddle_position[1]
+                and ball_y >= clients[1].paddle_position[1] - PADDLE_LENGTH
+                and ball_x >= clients[1].paddle_position[0]
+                and ball_x <= clients[1].paddle_position[0] + PADDLE_WIDTH):
             return 1
-        elif (ball_y <= self.client_positions[2][1]
-                and ball_y >= self.client_positions[2][1] - PADDLE_WIDTH
-                and ball_x <= self.client_positions[2][0]
-                and ball_x >= self.client_positions[2][0] - PADDLE_LENGTH):
+        elif (ball_y <= clients[2].paddle_position[1]
+                and ball_y >= clients[2].paddle_position[1] - PADDLE_WIDTH
+                and ball_x <= clients[2].paddle_position[0]
+                and ball_x >= clients[2].paddle_position[0] - PADDLE_LENGTH):
             return 2
-        elif (ball_y >= self.client_positions[3][1]
-                and ball_y <= self.client_positions[3][1] + PADDLE_WIDTH
-                and ball_x >= self.client_positions[3][0]
-                and ball_x <= self.client_positions[3][0] + PADDLE_LENGTH):
+        elif (ball_y >= clients[3].paddle_position[1]
+                and ball_y <= clients[3].paddle_position[1] + PADDLE_WIDTH
+                and ball_x >= clients[3].paddle_position[0]
+                and ball_x <= clients[3].paddle_position[0] + PADDLE_LENGTH):
             return 3
         return None
 
     async def broadcast_updates(self):
         """Broadcast updates to each connected client."""
         broadcast_info = {
-            'type': 'broadcast',
-            'data': {
-                'paddles': self.client_positions,
-                'scores': self.client_scores,
-                'ball': self.ball_position,
-                'bounce': self.ball_bounced,
+            "type": "broadcast",
+            "data": {
+                "players": [client.to_json() for client in self.active_clients.values()],
+                "ball": self.ball_position,
+                "bounce": self.ball_bounced,
             }
         }
-        websockets.broadcast(self.active_clients.values(), json.dumps(broadcast_info))
+        websockets.broadcast([player.connection for player in self.active_clients.values()], json.dumps(broadcast_info))
+
+    def client_from_side(self, side):
+        """Get the client that is on the specified side"""
+        for client in self.active_clients.values():
+            if client.player_number == side:
+                return client
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server = Server()
     asyncio.run(server.main())
