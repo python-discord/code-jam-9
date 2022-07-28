d350d3a16a69ca4555f9105836b17e711b32fac0
standard-affect <standardaffect3@gmail.com>
2022-07-28 14:47:31 -0400

    Add 2 questions

--- question_tests.py
+++ question_tests.py
@@ -42,3 +42,25 @@ def tuple_trickery():
     # y = list(x)
     x[0].pop()
     min(y[0])
+
+
+def string_replace():
+    """Tests knowledge of string manipulation"""
+    x = "a1"
+    # x[1] = 2
+    # x.replace(1, 2),
+    # x = f"{x2}",
+    x.replace("1", "2")
+    # x = x.replace(1, 2)
+
+
+def list_concatenation():
+    """Tests knowledge of list concatenation"""
+    x = [1, 2, 3]
+    y = [4, 5]
+    # x.add(y)
+    # x.append(y)
+    # x[:] = y
+    # x = list(x, y)
+    x.extend(y)
+    print(x)
