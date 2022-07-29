4a971b2133012b79d4ae4dde9120df6faac8bb48
Morgan Jackson <morjack@siue.edu>
2022-07-29 16:44:23 -0500

    Player now select their server + error handling

--- client/public/client.js
+++ client/public/client.js
@@ -1,43 +1,86 @@
 console.log("Starting client");
+var socket = ""
 
-const socket = new WebSocket("ws://localhost:8081/");
-socket.addEventListener("open", function (event) {
-  console.log("Connected to server");
-  console.log(event);
-});
+function connectToServer() {
+  // Lockout User Input
+  document.getElementById('input-label').innerHTML = "Connecting...";
+  document.getElementById('input').disabled = true;
+  document.getElementById('input-button').disabled = true;
+  document.getElementById('error-field').innerHTML = "";
 
-socket.addEventListener("message", function (event) {
-  console.log("Message from server ", event.data);
+  // Process Address
+  address = document.getElementById('input').value;
+  if (address == "") {
+    address = "ws://localhost:8081";
+  } else if (address.slice(0,2) != "ws") {
+    address = "ws://" + address;
+  }
+  console.log("Connecting to ", address);
 
-  let message = JSON.parse(event.data);
-
-  if (message.event == "uname_request") {
-    document.getElementById('input-label').innerHTML = "Username:";
-    document.getElementById('input').disabled = false;
-    document.getElementById('input').value = "";
-    document.getElementById('input-button').disabled = false;
+  // Attempt Connection
+  try {
+    socket = new WebSocket(address);
+  } catch (e) {
+    console.log("Error connecting to server");
+    resetSetup();
+    document.getElementById('error-field').innerHTML = "Bad Address";
+    return;
   }
 
-  if (message.event == "ulimit_request") {
-    document.getElementById('input-label').innerHTML = "Max Players:";
-    document.getElementById('input').disabled = false;
-    document.getElementById('input').value = "";
-    document.getElementById('input-button').disabled = false;
+  socket.onerror = function(event){
+    console.log(event);
+    resetSetup();
+    document.getElementById('error-field').innerHTML = "Unable To Connect";
   }
 
-  if (message.event == "user_join" || message.event == "user_leave") {
-    document.getElementById('player-count').innerHTML = `${message.count}<br>`;
-    let playerListHtml = message.uname_list.map((player) => {
-      return `<li>${player}</li>`;
-    }).join('')
-    document.getElementById('player-list').innerHTML = playerListHtml;
-  }
+  // Verify connection
+  socket.addEventListener("open", function (event) {
+    console.log("Connected to server");
+    console.log(event);
+  });
 
-  if (message.error == "user limit has been reached"){
-    document.getElementById('input-label').innerHTML = "Server Full";
-  }
+  // Handle messages
+  socket.addEventListener("message", function (event) {
+    console.log("Message from server ", event.data);
 
-});
+    let message = JSON.parse(event.data);
+
+    if (message.event == "uname_request") {
+      document.getElementById('input-label').innerHTML = "Username:";
+      document.getElementById('input').disabled = false;
+      document.getElementById('input').value = "";
+      document.getElementById('input-button').disabled = false;
+    }
+
+    if (message.event == "ulimit_request") {
+      document.getElementById('input-label').innerHTML = "Max Players:";
+      document.getElementById('input').disabled = false;
+      document.getElementById('input').value = "";
+      document.getElementById('input-button').disabled = false;
+    }
+
+    if (message.event == "user_join" || message.event == "user_leave") {
+      document.getElementById('player-count').innerHTML = `${message.count}<br>`;
+      let playerListHtml = message.uname_list.map((player) => {
+        return `<li>${player}</li>`;
+      }).join('')
+      document.getElementById('player-list').innerHTML = playerListHtml;
+    }
+
+    if (message.error == "user limit has been reached"){
+      document.getElementById('error-field').innerHTML = "Server Full";
+      resetSetup();
+    }
+
+  });
+}
+
+function resetSetup () {
+  document.getElementById('input-label').innerHTML = "Server Address:";
+  document.getElementById('input').disabled = false;
+  document.getElementById('input').value = "";
+  document.getElementById('input-button').disabled = false;
+}
 
 function handleSetup() {
   let type = document.getElementById('input-label').innerHTML
@@ -45,6 +88,8 @@ function handleSetup() {
     sendUName();
   } else if (type == "Max Players:") {
     sendULimit();
+  } else if (type == "Server Address:") {
+    connectToServer();
   }
 }
 
@@ -54,6 +99,9 @@ function sendUName() {
   socket.send(JSON.stringify({
     "uname": uname
   }));
+  document.getElementById('input').disabled = true;
+  document.getElementById('input').value = "";
+  document.getElementById('input-button').disabled = true;
 }
 
 function sendULimit() {
@@ -62,6 +110,9 @@ function sendULimit() {
   socket.send(JSON.stringify({
     "ulimit": ulimit
   }));
+  document.getElementById('input').disabled = true;
+  document.getElementById('input').value = "";
+  document.getElementById('input-button').disabled = true;
 }
 
 function runCode() {
